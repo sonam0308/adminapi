6b01b85225653306701e4cf4915995300cd304c6
deepakverma91123 <deepakverma91123@gmail.com>
2022-02-17 20:51:59 +0530

    update

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const bodyparser = require('body-parser')
+const cors = require('cors')
 const dotenv = require("dotenv")
 dotenv.config()
 require('./db/config')
@@ -7,6 +9,20 @@ require('./db/config')
 const userroutes = require('./routes/user');
 const leadroutes = require('./routes/leads')
 app.use(express.json())
+
+
+
+// parse application/json
+app.use(bodyparser.json())
+app.use(express.urlencoded({ extended: false }))
+// app.use(cors({
+//     origin: ``,
+//     credentials: true
+// }))
+app.use(cors());
+
+
+
 
 app.get('/', (req, res) => {
     res.json({
